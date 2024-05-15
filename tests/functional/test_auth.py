e9581f430dfebbebd3d4550a194c97cf93133f50
Matthew <mchow123@my.bcit.ca>
2024-05-15 10:09:41 -0700

    add more to test_auth

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -26,30 +26,35 @@ def test_register(client):
         response = client.get("/auth/register")
         assert response.status_code ==  200
 
-@pytest.fixture(scope="module")
-def successful_register_post():
-    url = "http://localhost:8888/auth/register"
-    data = {"email": "Jimbob42@test.ca", "name": "Jimbob", "password": "password"}
-    response = requests.post(url, data=data)
-    return response
-
-def test_successful_register_post(successful_register_post):
-    assert successful_register_post.status_code == 200  # Assuming successful registration returns status code 200
-
 # @pytest.fixture(scope="module")
-# def fail_register_post():
+# def successful_register_post():
 #     url = "http://localhost:8888/auth/register"
 #     data = {"email": "Jimbob42@test.ca", "name": "Jimbob", "password": "password"}
 #     response = requests.post(url, data=data)
 #     return response
 
+# def test_successful_register_post(successful_register_post):
+#     assert successful_register_post.status_code == 200  # Assuming successful registration returns status code 200
+
+
+
 def test_fail_register_post():
     url = "http://localhost:8888/auth/register"
-    # data = {"email": "Jimbob42@test.ca", "name": "Jimbob", "password": "password"}
-    # response = requests.post(url, data=data)
-    # assert successful_register_post.status_code == 302
-    # # assert "email": 
-    # return response
+    # Send a registration request with incomplete or invalid data
+    data = {"email": "", "name": "Critize", "password": "password"}  # Empty email
+    response = requests.post(url, data=data)
+    # Assert that the status code of the response does not indicate a successful registration
+    assert response.status_code != 200
+    data = {"email": "Critize@test.ca", "name": "", "password": "password"}  # Empty email
+    response = requests.post(url, data=data)
+    # Assert that the status code of the response does not indicate a successful registration
+    assert response.status_code != 200
+    data = {"email": "Critize", "name": "Critize", "password": ""}  # Empty email
+    response = requests.post(url, data=data)
+    # Assert that the status code of the response does not indicate a successful registration
+    assert response.status_code != 200
+    
+
 
 
 
