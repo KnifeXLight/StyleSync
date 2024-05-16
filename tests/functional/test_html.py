26cbae0eada9643af19d237d381d8f0bd64749bb
Charley Liao <c24liao@gmail.com>
2024-05-16 15:57:13 -0700

    adjust test html.py

--- tests/functional/test_html.py
+++ tests/functional/test_html.py
@@ -1,77 +1,97 @@
+from math import log
+from flask import session, url_for
 import pytest
 import sys
 import os
 
 # Add the app_folder to the system path to allow imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from flask import session
-from app import app
-from models import User
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../')))
 
-@pytest.fixture(scope='module')
+from db import db
+from models import User
+from app import app
+
+
+@pytest.fixture
 def client():
     app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+    app.config['WTF_CSRF_ENABLED'] = False
+    client = app.test_client()
 
-@pytest.fixture(scope='module')
+    with app.app_context():
+        db.create_all()
+        yield client
+        db.drop_all()
+
+
+@pytest.fixture
 def logged_in_client(client):
-    # Simulate user login
-    with client.session_transaction() as sess:
-        sess['user_id'] = 1  # Assuming user ID is 1
+    # Create a test user
+    user = User(name='Test User', email='test@example.com',
+                password='password')
+    db.session.add(user)
+    db.session.commit()
+    # Log in the test user; Simulate user login
+    client.post('/auth/login', data={'email': 'test@example.com',
+                'password': 'password'}, follow_redirects=True)
     return client
 
 
-# This should redirct you to login if you type in url without login.
-def test_home_redirct(client):
+# This should redirect you to login if you type in url without login.
+def test_home_redirect(client):
     response = client.get('/views/home', follow_redirects=True)
-    assert response.status_code == 200  # Assuming the login page redirects to the home page after successful login
-    assert b'StyleSynctest' in response.data  # Adjust this assertion based on the content of your home page
-"""
-in base.html in auth folder, i changed the title to match the assert data. 
-If you change it back to StyleSync, it should fail test and display redirect page.
-Look and see and change the title to check.
-"""
+    # Assuming the login page redirects to the home page after successful login
+    assert response.status_code == 200
+
 
 # This test checks if login is successful, and should direct you to homepage
 def test_user_login_and_homepage(client):
-    # I hardcoded it so it logins in with my cred, need to change this
-    response = client.post('/auth/login', data={'email': 'kms@test.ca', 'password': 'HAHAXD123'}, follow_redirects=True)
-    
+    response = client.post(
+        '/auth/login', data={'email': 'test@example.com', 'password': 'password'}, follow_redirects=True)
     assert response.status_code == 200
-    assert response.status_code != 302  # Checks if login fails and redirects you to login page
-    assert b'StyleSynckk' in response.data  
-"""
-Same as previous except check base.html in html folder. Title currently is StyleSynckk
-Change title for errors. Remember that login data is hardcoded to my info, need to change that
-WTF, if i delete this, the tests below do not work? It makes sense somewhat
-"""
+    # Checks if login fails and redirects you to login page
+    assert response.status_code != 302
+
+
+# def test_access_protected_route_wardrobe(logged_in_client):
+#     response = logged_in_client.get('/views/wardrobe')
+#     assert response.status_code == 200
+#     # Checks if login fails and redirects you to login page
+#     assert response.status_code != 302
+#     assert b'StyleSynckk' in response.data
+
 
 def test_access_protected_route_wardrobe(logged_in_client):
     response = logged_in_client.get('/views/wardrobe')
     assert response.status_code == 200
-    assert response.status_code != 302  # Checks if login fails and redirects you to login page
-    assert b'StyleSynckk' in response.data
 """
 Same as previous except check base.html in html folder. Title currently is StyleSynckk
 Change title for errors. Remember that login data is hardcoded to my info, need to change that
 """
+
 
 def test_access_protected_route_homepage(logged_in_client):
     response = logged_in_client.get('/views/home')
     assert response.status_code == 200
-    assert response.status_code != 302  # Checks if login fails and redirects you to login page
-    assert b'StyleSynckk' in response.data
+    # Checks if login fails and redirects you to login page
+    assert response.status_code != 302
+
+
 """
 Same as previous except check base.html in html folder. Title currently is StyleSynckk
 Change title for errors. Remember that login data is hardcoded to my info, need to change that
 """
 
+
 def test_access_protected_route_newoutfit(logged_in_client):
     response = logged_in_client.get('/views/newoutfit')
     assert response.status_code == 200
-    assert response.status_code != 302  # Checks if login fails and redirects you to login page
+    # Checks if login fails and redirects you to login page
+    assert response.status_code != 302
     assert b'StyleSynckk' in response.data
+
+
 """
 Same as previous except check base.html in html folder. Title currently is StyleSynckk
 Change title for errors. Remember that login data is hardcoded to my info, need to change that
