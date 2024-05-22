c070270eb393ccbc752539423a94f33dcdee56e9
Matthew <mchow123@my.bcit.ca>
2024-05-22 09:59:15 -0700

    add test to html

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from app import create_app
 # Add the app_folder to the system path to allow imports
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from db import db
-from models import User
+from models import User, Item
 
 @pytest.fixture
 def app():
@@ -42,6 +42,20 @@ def logged_in_client(client, app):
                 sess['user_id'] = test_user.id
     
     return client
+
+@pytest.fixture
+def test_item(client, app):
+    with client:
+        with app.app_context():
+            item = Item(name="Test Item", user_id=1, image_url="test_image_url")
+            db.session.add(item)
+            db.session.commit()
+            print(item.image_url)
+            print(item.name)
+            print(item.id)
+            yield item
+            # db.session.delete(item)
+            # db.session.commit()
 
 @pytest.fixture
 def failed_login_password(client, app):
--- tests/functional/test_html.py
+++ tests/functional/test_html.py
@@ -1,3 +1,5 @@
+from models import Item
+from db import db
 
 # * Test Access to Protected Routes
 def test_access_protected_route_wardrobe(logged_in_client):
@@ -48,3 +50,34 @@ def test_wardrobe_not_logged_in(client):
     with client:
         response = client.get('/views/wardrobe', follow_redirects=False)
         assert response.status_code == 302  # Redirect to login
+
+def test_item_route_non_existing(logged_in_client):
+    with logged_in_client:
+        response = logged_in_client.get('/views/item/999', follow_redirects=True)
+        assert response.status_code == 200
+# Test if Item is not accessible when not logged in
+def test_item_route_not_logged_in(client):
+    with client:
+        response = client.get('/views/item/1', follow_redirects=False)
+        assert response.status_code == 302
+
+# Test the delete_item route for non-existing item
+def test_delete_item_route_non_existing(logged_in_client):
+    with logged_in_client:
+        response = logged_in_client.get('/views/items/999', follow_redirects=True)
+        assert response.status_code == 404
+
+def test_delete_item_route_existing(logged_in_client, test_item):
+    # Delete the test item
+    # with logged_in_client:
+        item_id = test_item.id
+        # print("Item ID:", item_id)
+        assert db.session.query(Item).filter_by(id=item_id).first() is not None
+        response = logged_in_client.get(f'/views/items/{item_id}', follow_redirects=True)
+            
+            # Verify that the item exists
+        assert response.status_code == 200
+        item = db.session.query(Item).filter_by(id=item_id).first()
+            # Verify that the item has been deleted
+        assert item is None
+        # assert db.session.query(Item).filter_by(id=item_id).first() is None
