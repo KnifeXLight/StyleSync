3ade617f60de8380110a681134a56d875dfe7aa2
Charley Liao <c24liao@gmail.com>
2024-05-23 15:48:41 -0700

    fix auth.py test workflow

--- routes/auth.py
+++ routes/auth.py
@@ -3,11 +3,7 @@ from db import db
 from models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
-# from flask_mail import Mail, Message
 from time import time
-# import jwt
-# import os
-
 
 
 
